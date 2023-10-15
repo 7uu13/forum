32ac632da5ebab310a567bef93fb1999cc81ebd5
Karl-Thomas <karl-thomas@zink.ee>
2023-10-15 16:47:46 +0300

    Revert "Merge branch 'main' of https://github.com/7uu13/forum into main"
    This reverts commit ade8e08bc1e2d036345de438224bcf53a74c438e, reversing
    changes made to b5e6bc37fc32b590a09db1dccced7a67a16afce1.

--- controller/homePageContoller.go
+++ controller/homePageContoller.go
@@ -2,61 +2,35 @@ package controller
 
 import (
 	"database/sql"
-	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
+	"encoding/json"
+	"errors"
 
+	"github.com/7uu13/forum/middleware"
 	"github.com/7uu13/forum/dto"
 	"github.com/7uu13/forum/model"
 	"github.com/7uu13/forum/service"
+
 )
 
 func HomePage(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 
 	if r.URL.Path != "/" {
-		tmpl, err := template.ParseGlob("templates/notFound.html")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = tmpl.Execute(w, r)
+		http.NotFound(w, r)
 		return
 	}
 
-	category_slug := r.URL.Query().Get("category")
-	category, err := GetCurrentCategory(db, category_slug)
-
-	if err != nil {
-		// If category not found, return not found html
-		tmpl, err := template.ParseGlob("templates/notFound.html")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = tmpl.Execute(w, r)
-		return
-	}
-
-	if category.Id == 0 {
-		// TODO: If category is not selected, select first category from all categories
-	}
-
-	// Get all categories for topics sidebar
 	categories, err := service.GetCategories(db)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	data := struct {
-		Categories      []model.Categories
-		CurrentCategory model.Categories
+		Categories []model.Categories
 	}{
-		Categories:      categories,
-		CurrentCategory: category,
+		Categories: categories,
 	}
-
-	tmpl, err := template.ParseGlob("templates/home.html")
+	
+	tmpl, err := template.ParseGlob("templates/home2.html")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,22 +46,6 @@ func HomePage(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 func Profilepage(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 	// Step 1: Authenticate the user and retrieve session information.
 	_, username, user, err := GetSessionInfo(db, w, r)
-}
-
-func GetCurrentCategory(db *sql.DB, category string) (model.Categories, error) {
-	if category != "" {
-		categories, err := service.GetCategoryBySlug(db, category)
-		if err != nil || len(categories) == 0 {
-			return model.Categories{}, err
-		}
-		return categories[0], nil
-	}
-	return model.Categories{}, nil
-}
-
-func Profilepage(w http.ResponseWriter, r *http.Request) {
-
-	c, err := r.Cookie("test")
 	if err != nil {
 		return
 	}
@@ -105,7 +63,7 @@ func Profilepage(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Invalid JSON", http.StatusBadRequest)
 			return
 		}
-
+		
 		deleteAccount := r.FormValue("Delete-confirm")
 		if deleteAccount == "DELETE" {
 			userID, _ := service.DeleteUser(db, username)
@@ -126,7 +84,7 @@ func renderProfilePage(w http.ResponseWriter, templatePath string, data interfac
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	
 	err = tmpl.Execute(w, data)
 	if err != nil {
 		log.Fatal(err)
--- service/postService.go
+++ service/postService.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"database/sql"
-	"fmt"
 
 	"github.com/7uu13/forum/model"
 )
@@ -26,35 +25,4 @@ func CreatePost(db *sql.DB, post model.Post) (int64, error) {
 	}
 
 	return postID, nil
-}
-
-func GetCategoryPosts(db *sql.DB, category model.Categories) ([]model.Post, error) {
-	stmt := `
-	SELECT * FROM posts_category
-	INNER JOIN posts ON posts_categories.post_id = posts.id
-	`
-	var posts []model.Post
-	res, err := db.Query(stmt)
-	if err != nil {
-		panic(err)
-	}
-
-	defer res.Close()
-
-	for res.Next() {
-		var post model.Post
-		err = res.Scan(&post.Id, &post.Title, &post.Content, &post.Created, &post.UserId)
-		if err != nil {
-			panic(err)
-		}
-
-		posts = append(posts, post)
-	}
-
-	if err := res.Err(); err != nil {
-		return nil, err
-	}
-
-	fmt.Println(posts)
-	return posts, nil
 }
