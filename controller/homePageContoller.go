3a241e386dc58cda909f98d4d282d041329fe9f8
Karl-Thomas <karl-thomas@zink.ee>
2023-10-16 13:51:12 +0300

    Homepage controller

--- controller/homePageContoller.go
+++ controller/homePageContoller.go
@@ -13,6 +13,18 @@ type HomePageController struct{}
 var category types.Categories
 
 func (_ *HomePageController) HomePage(w http.ResponseWriter, r *http.Request) {
+	// Define the data structure to pass to templates
+	data := struct {
+		Categories      []types.Categories
+		CurrentCategory types.Categories
+		Posts           []types.Post
+	}{
+		Categories:      []types.Categories{},
+		CurrentCategory: types.Categories{},
+		Posts:           []types.Post{},
+	}
+
+	// Check if the URL path is not root, return not found template
 	if r.URL.Path != "/" {
 		tmpl, err := template.ParseGlob("ui/templates/notFound.html")
 		if err != nil {
@@ -23,46 +35,63 @@ func (_ *HomePageController) HomePage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	category_slug := r.URL.Query().Get("category")
-	category, err := category.GetCurrentCategory(category_slug)
+	// Get all categories for the topics sidebar
+	categories, err := category.GetCategories()
 	if err != nil {
-		// agh
-		// If category not found, return not found html
-		tmpl, err := template.ParseGlob("ui/templates/notFound.html")
+		log.Println(err)
+	}
+	data.Categories = categories
+
+	postID := r.URL.Query().Get("post")
+	categorySlug := r.URL.Query().Get("category")
+
+	if postID != "" {
+		post, err := post.GetPostById(postID)
+
 		if err != nil {
-			log.Fatal(err)
+			renderNotFoundTemplate(w, r)
+			return
 		}
 
-		err = tmpl.Execute(w, r)
-		return
-	}
+		data.Posts = append(data.Posts, post)
+		renderTemplate("ui/templates/post.html", w, data)
 
-	if category.Id == 0 {
-		// TODO: If category is not selected, select first category from all categories
-	}
+	} else if categorySlug != "" {
+		category, err := category.GetCurrentCategory(categorySlug)
 
-	// Get all categories for topics sidebar
-	categories, err := category.GetCategories()
+		if err != nil || category.Id == 0 {
+			renderNotFoundTemplate(w, r)
+			return
+		}
+
+		data.CurrentCategory = category
+		posts, err := post.GetCategoryPosts(category)
+
+		if err != nil || len(posts) == 0 {
+			log.Println(err)
+		}
+
+		data.Posts = posts
+		renderTemplate("ui/templates/home.html", w, data)
+	} else {
+		renderTemplate("ui/templates/home.html", w, data)
+	}
+}
+
+func renderNotFoundTemplate(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseGlob("ui/templates/notFound.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	data := struct {
-		Categories      []types.Categories
-		CurrentCategory types.Categories
-	}{
-		Categories:      categories,
-		CurrentCategory: category,
-	}
+	err = tmpl.Execute(w, r)
+}
 
-	tmpl, err := template.ParseGlob("ui/templates/home.html")
+func renderTemplate(templatePath string, w http.ResponseWriter, data interface{}) {
+	tmpl, err := template.ParseGlob(templatePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	err = tmpl.Execute(w, data)
-
-	if err != nil {
-		log.Fatal(err)
-	}
 }
